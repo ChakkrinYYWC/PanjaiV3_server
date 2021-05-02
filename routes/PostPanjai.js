756ac23693ee4f03da4d4c3827a94122f53aae3e
TBoonyapat <boonyapat.tawan@gmail.com>
2021-05-02 21:56:10 +0700

    .

--- routes/PostPanjai.js
+++ routes/PostPanjai.js
@@ -28,7 +28,7 @@ const storage = new CloudinaryStorage({
 
 const uploadCloud = multer({ storage: storage });
 
-router.get('/', (req, res) => {
+router.get('/test', (req, res) => {
     console.log('*')
     PostPanjai.find({}, (err, docs) => {
         if (!err){
